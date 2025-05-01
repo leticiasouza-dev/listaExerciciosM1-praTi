ec8437c5ce61ab2e112b196177f260a80bbd9657
leticiaSouzaDev <lec667121@gmail.com>
2025-05-01 12:58:37 -0300

    feat: exercicio 1 resolvido

--- exercicios01.js
+++ exercicios01.js
@@ -1,1 +1,12 @@
-// 
+// 1. Escreva um programa que recebe um número inteiro e verifica se ele é par ou ímpar utilizando uma estrutura de controle if.
+
+import entradaDeDados from 'readline-sync'
+
+console.log('--------------------BEM-VINDO AO VERIFICADOR DE VALORes PARES E IMPARES--------------------')
+let valor = entradaDeDados.questionInt('Digite um valor do tipo inteiro: ')
+
+if(valor % 2 == 0){
+    console.log('Esse valor é Par')
+} else{ 
+    console.log('Esse valor é ímpar')
+}
